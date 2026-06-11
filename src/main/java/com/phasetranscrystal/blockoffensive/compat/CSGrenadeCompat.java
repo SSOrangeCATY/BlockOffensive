014c97b7558a8fb5df5a296d95c39c09c9148ee4
SSOrangeCATY <jumaoz@qq.com>
2026-06-11 17:45:45 +0800

    fix: avoid csgrenades moditems linkage

--- src/main/java/com/phasetranscrystal/blockoffensive/compat/CSGrenadeCompat.java
+++ src/main/java/com/phasetranscrystal/blockoffensive/compat/CSGrenadeCompat.java
@@ -2,12 +2,12 @@ package com.phasetranscrystal.blockoffensive.compat;
 
 import club.pisquad.minecraft.csgrenades.entity.CounterStrikeGrenadeEntity;
 import club.pisquad.minecraft.csgrenades.event.GrenadeThrowEvent;
-import club.pisquad.minecraft.csgrenades.registry.ModItems;
 import com.phasetranscrystal.blockoffensive.client.BOClientEvent;
 import com.phasetranscrystal.blockoffensive.util.BOUtil;
 import com.phasetranscrystal.blockoffensive.util.ThrowableType;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.item.Item;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -15,26 +15,47 @@ import net.minecraftforge.registries.ForgeRegistries;
 import java.util.Map;
 
 public class CSGrenadeCompat {
+    private static final ResourceLocation HE_GRENADE = ResourceLocation.fromNamespaceAndPath("csgrenades", "hegrenade");
+    private static final ResourceLocation HE_GRENADE_T = ResourceLocation.fromNamespaceAndPath("csgrenades", "hegrenade_t");
+    private static final ResourceLocation INCENDIARY = ResourceLocation.fromNamespaceAndPath("csgrenades", "incendiary");
+    private static final ResourceLocation INCENDIARY_T = ResourceLocation.fromNamespaceAndPath("csgrenades", "incendiary_t");
+    private static final ResourceLocation MOLOTOV = ResourceLocation.fromNamespaceAndPath("csgrenades", "molotov");
+    private static final ResourceLocation MOLOTOV_T = ResourceLocation.fromNamespaceAndPath("csgrenades", "molotov_t");
+    private static final ResourceLocation SMOKE_GRENADE = ResourceLocation.fromNamespaceAndPath("csgrenades", "smokegrenade");
+    private static final ResourceLocation SMOKE_GRENADE_T = ResourceLocation.fromNamespaceAndPath("csgrenades", "smokegrenade_t");
+    private static final ResourceLocation FLASH_BANG = ResourceLocation.fromNamespaceAndPath("csgrenades", "flashbang");
+    private static final ResourceLocation FLASH_BANG_T = ResourceLocation.fromNamespaceAndPath("csgrenades", "flashbang_t");
+    private static final ResourceLocation DECOY = ResourceLocation.fromNamespaceAndPath("csgrenades", "decoy");
+    private static final ResourceLocation DECOY_T = ResourceLocation.fromNamespaceAndPath("csgrenades", "decoy_t");
 
     public static void registerKillIcon(Map<ResourceLocation, String> registry){
-        ModItems instance = ModItems.INSTANCE;
-        registry.put(ForgeRegistries.ITEMS.getKey(instance.getHEGRENADE_ITEM().get()),"grenade");
-        registry.put(ForgeRegistries.ITEMS.getKey(instance.getINCENDIARY_ITEM().get()),"ct_incendiary_grenade");
-        registry.put(ForgeRegistries.ITEMS.getKey(instance.getMOLOTOV_ITEM().get()),"t_incendiary_grenade");
-        registry.put(ForgeRegistries.ITEMS.getKey(instance.getSMOKE_GRENADE_ITEM().get()),"smoke_shell");
-        registry.put(ForgeRegistries.ITEMS.getKey(instance.getFLASH_BANG_ITEM().get()),"flash_bomb");
+        registerKillIcon(registry, HE_GRENADE, "grenade");
+        registerKillIcon(registry, HE_GRENADE_T, "grenade");
+        registerKillIcon(registry, INCENDIARY, "ct_incendiary_grenade");
+        registerKillIcon(registry, INCENDIARY_T, "ct_incendiary_grenade");
+        registerKillIcon(registry, MOLOTOV, "t_incendiary_grenade");
+        registerKillIcon(registry, MOLOTOV_T, "t_incendiary_grenade");
+        registerKillIcon(registry, SMOKE_GRENADE, "smoke_shell");
+        registerKillIcon(registry, SMOKE_GRENADE_T, "smoke_shell");
+        registerKillIcon(registry, FLASH_BANG, "flash_bomb");
+        registerKillIcon(registry, FLASH_BANG_T, "flash_bomb");
     }
 
     public static void init() {
         MinecraftForge.EVENT_BUS.register(CSGrenadeCompat.class);
 
-        ModItems items = ModItems.INSTANCE;
-        BOUtil.registerThrowable(ThrowableType.FLASH_BANG, items.getFLASH_BANG_ITEM().get());
-        BOUtil.registerThrowable(ThrowableType.DECOY, items.getDECOY_GRENADE_ITEM().get());
-        BOUtil.registerThrowable(ThrowableType.GRENADE, items.getHEGRENADE_ITEM().get());
-        BOUtil.registerThrowable(ThrowableType.SMOKE, items.getSMOKE_GRENADE_ITEM().get());
-        BOUtil.registerThrowable(ThrowableType.INCENDIARY_GRENADE, items.getMOLOTOV_ITEM().get());
-        BOUtil.registerThrowable(ThrowableType.INCENDIARY_GRENADE, items.getINCENDIARY_ITEM().get());
+        registerThrowable(ThrowableType.FLASH_BANG, FLASH_BANG);
+        registerThrowable(ThrowableType.FLASH_BANG, FLASH_BANG_T);
+        registerThrowable(ThrowableType.DECOY, DECOY);
+        registerThrowable(ThrowableType.DECOY, DECOY_T);
+        registerThrowable(ThrowableType.GRENADE, HE_GRENADE);
+        registerThrowable(ThrowableType.GRENADE, HE_GRENADE_T);
+        registerThrowable(ThrowableType.SMOKE, SMOKE_GRENADE);
+        registerThrowable(ThrowableType.SMOKE, SMOKE_GRENADE_T);
+        registerThrowable(ThrowableType.INCENDIARY_GRENADE, MOLOTOV);
+        registerThrowable(ThrowableType.INCENDIARY_GRENADE, MOLOTOV_T);
+        registerThrowable(ThrowableType.INCENDIARY_GRENADE, INCENDIARY);
+        registerThrowable(ThrowableType.INCENDIARY_GRENADE, INCENDIARY_T);
     }
 
     public static boolean is(Entity entity){
@@ -47,6 +68,19 @@ public class CSGrenadeCompat {
             event.setCanceled(true);
         }else{
             BOUtil.buildGrenadeMessageAndSend(event.getItemStack());
+        }
+    }
+
+    private static void registerKillIcon(Map<ResourceLocation, String> registry, ResourceLocation itemId, String iconId) {
+        if (ForgeRegistries.ITEMS.containsKey(itemId)) {
+            registry.put(itemId, iconId);
+        }
+    }
+
+    private static void registerThrowable(ThrowableType type, ResourceLocation itemId) {
+        Item item = ForgeRegistries.ITEMS.getValue(itemId);
+        if (item != null) {
+            BOUtil.registerThrowable(type, item);
         }
     }
 }
